e883d104b86f6c2a71bc5c6abef2d3b3f6a89b18
Luis Cruz Sanchez <luis.f.cruzsanchez@outlook.com>
2024-12-08 13:11:27 -0800

    initial commit

---
+++ Models/GameItem.cs
@@ -0,0 +1,11 @@
+﻿namespace MatchingGameApp.Models
+{
+    public class GameItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } // Name of the item (e.g., "Dog")
+        public string ImageUrl { get; set; } // URL for the item's image
+        public string Category { get; set; } // Category for sorting
+    }
+
+}
